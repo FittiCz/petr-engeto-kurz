dfa46d3410d4f05fc06df8f88c63bf7019c47a7a
Petr Kovalský <mailodpietra@gmail.com>
2024-07-16 15:46:47 +0200

    import switch

---
+++ Projekt_3/script.js
@@ -0,0 +1,8 @@
+const switcher = document.querySelector(".switcher input")
+
+switcher.addEventListener("change", () => {
+    console.log("test");
+    // if () {
+    //     // tady změním na darkmod
+    // }
+})
