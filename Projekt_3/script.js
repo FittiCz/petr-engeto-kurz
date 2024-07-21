024d7fb120a32b1aa7a80d0cc159490893c53ce4
Petr Kovalský <mailodpietra@gmail.com>
2024-07-21 12:55:33 +0200

    Script na motiv

--- Projekt_3/script.js
+++ Projekt_3/script.js
@@ -1,8 +1,20 @@
-const switcher = document.querySelector(".switcher input")
+const switcher = document.querySelector(".toggle input")
 
-switcher.addEventListener("change", () => {
-    console.log("test");
-    // if () {
-    //     // tady změním na darkmod
-    // }
-})
+switcher.addEventListener("change", (event) => {
+    if (event.target.checked) {
+        document.documentElement.setAttribute("data-theme", "dark")
+
+    }
+    else {
+        document.documentElement.setAttribute("data-theme", "light")
+    }
+});
+
+
+const toggle = document.querySelector('.toggle-input');
+const initialState = localStorage.getItem('toggleState') == 'true';
+toggle.checked = initialState;
+
+toggle.addEventListener('change', function () {
+    localStorage.setItem('toggleState', toggle.checked);
+});
